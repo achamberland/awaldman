62f53995b9bb2db21c14c902569efc53a0966ed9
root <root@solopage-ubuntu-512mb-tor1-01.localdomain>
2017-03-29 01:41:08 +0000

    Set dev env to port 80 temporarily to use as prod env - domain is set up

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,8 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :v2, V2.Endpoint,
-  http: [port: 4000],
+#  http: [port: 4000],
+  http: [port: 80],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
